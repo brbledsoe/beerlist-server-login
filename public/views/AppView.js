49dea23a2787590fa1a56f9dccc276b66987a7ce
Brandon Bledsoe <brbledsoe.m@gmail.com>
2016-05-01 14:57:55 +0300

    add view stuff

--- public/views/AppView.js
+++ public/views/AppView.js
@@ -15,19 +15,32 @@ var AppView = Backbone.View.extend({
 
     this.$beersContainer = this.$('.beers-container');
     this.$reviewsContainer = this.$('.reviews-container');
+    this.$registerContainer = this.$('.register-container');
 
     this.listenTo(this.model.get('beers'), 'add', this.addBeer);
     this.listenTo(this.model.get('beers'), 'reset', this.renderBeers);
 
-    this.listenTo(this.model, 'change:show_reviews', this.renderView);
+    this.listenTo(this.model, 'change:view', this.renderView);
     this.listenTo(this.model, 'change:current_beer', this.renderDetailView);
 
     this.detailView = null;
   },
 
   renderView: function () {
-    this.$reviewsContainer.toggleClass('show', this.model.get('show_reviews'));
-    this.$beersContainer.toggleClass('show', !this.model.get('show_reviews'));
+
+    var view = this.model.get('view');
+
+    var viewMap = {
+      'beers': this.$beersContainer,
+      'reviews': this.$reviewsContainer,
+      'register': this.$registerContainer
+    };
+
+    for(var eachView in viewMap){
+      viewMap[eachView].removeClass('show');
+    }
+
+    viewMap[view].addClass('show');
   },
 
   renderDetailView: function () {
